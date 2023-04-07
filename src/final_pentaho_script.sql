9b17a22435fb9573b3c71a1aa4a04489e4a5d47d
Carlos Bracho <carlosebr93@outlook.com>
2023-04-06 21:38:23 -0400

    fix pentaho project and add script

---
+++ src/final_pentaho_script.sql
@@ -0,0 +1,11 @@
+create table if not exists pentaho_ETLDB (
+    id_trades int,
+    trade_usd float,
+    kg float,
+    quantity float,
+    id_code int,
+    id_country int,
+    id_flow int,
+    id_year int,
+    id_quantity int
+);
